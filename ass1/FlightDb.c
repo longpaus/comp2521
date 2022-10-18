4f3727b1a6a96cfe75e0146ac7f481048efaf8b3
Hoang Long <longpaus@gmail.com>
2022-10-18 13:11:08 +1100

    did insertDb

--- ass1/FlightDb.c
+++ ass1/FlightDb.c
@@ -8,39 +8,63 @@
 #include "List.h"
 #include "Tree.h"
 
-struct flightDb {
 
+int compareByFlightNum(Record r1, Record r2);
+
+struct flightDb {
+    Tree byFlightNum
 };
 
 FlightDb DbNew(void) {
-    return NULL;
+    FlightDb db = malloc(sizeof(*db));
+    db ->byFlightNum = TreeNew(compareByFlightNum);
+	return db;
 }
-
-void DbFree(FlightDb db) {
-
-}
+void DbFree(FlightDb db) {}
 
 bool DbInsertRecord(FlightDb db, Record r) {
-    return false;
+	return TreeInsert(db ->byFlightNum,r);
 }
 
 List DbFindByFlightNumber(FlightDb db, char *flightNumber) {
-    return ListNew();
+	return ListNew();
 }
 
-List DbFindByDepartureAirportDay(FlightDb db, char *departureAirport,
-                                 int day) {
-    return ListNew();
+List DbFindByDepartureAirportDay(FlightDb db, char *departureAirport, int day) {
+	return ListNew();
 }
 
-List DbFindBetweenTimes(FlightDb db, 
-                        int day1, int hour1, int min1, 
-                        int day2, int hour2, int min2) {
-    return ListNew();
+List DbFindBetweenTimes(FlightDb db, int day1, int hour1, int min1, int day2,
+                        int hour2, int min2) {
+	return ListNew();
 }
 
-Record DbFindNextFlight(FlightDb db, char *flightNumber, 
-                        int day, int hour, int min) {
-    return NULL;
+Record DbFindNextFlight(FlightDb db, char *flightNumber, int day, int hour,
+                        int min) {
+	return NULL;
 }
 
+/*
+compare two records r1 and r2.
+comparing by flight number then depature day then hour and then minute.
+returns 1 if r1 > r2, 0 if r1 == r2 and -1 if r1 < r2.
+*/
+
+int compareByFlightNum(Record r1, Record r2) {
+	int flightNumCompare =
+	    strcmp(RecordGetFlightNumber(r1), RecordGetFlightNumber(r2));
+	if (flightNumCompare != 0) {
+		return (flightNumCompare > 0) ? 1 : -1;
+	}
+    if(RecordGetDepartureDay(r1) != RecordGetDepartureDay(r2)){
+        return(RecordGetDepartureDay(r1) > RecordGetDepartureDay(r2)) ? 1 : -1;
+    }
+    if(RecordGetDepartureHour(r1) != RecordGetDepartureHour(r2)){
+        return (RecordGetDepartureHour(r1) > RecordGetDepartureHour(r2)) ? 1 : -1;
+    }
+    if(RecordGetDepartureMinute(r1) != RecordGetDepartureMinute(r2)){
+        return (RecordGetDepartureMinute(r1) > RecordGetDepartureMinute(r2)) ? 1 : -1;
+    }
+    
+    return 0;
+}
