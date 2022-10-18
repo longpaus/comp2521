328d1b98d1d88c8ef0a150c15d2f893d889f7bdd
Hoang Long <longpaus@gmail.com>
2022-10-18 22:56:43 +1100

    fix

--- ass1/FlightDb.c
+++ ass1/FlightDb.c
@@ -32,8 +32,8 @@ bool DbInsertRecord(FlightDb db, Record r) {
 List DbFindByFlightNumber(FlightDb db, char *flightNumber) {
     Record low = RecordNew(flightNumber,"","",0,0,0,90);
     Record high = RecordNew(flightNumber,"","",6,23,59,90);
-	List l = ListNew();
-    return TreeSearchBetween(db->byFlightNum,low,high);
+    List l = TreeSearchBetween(db->byFlightNum,low,high);
+    
 }
 
 List DbFindByDepartureAirportDay(FlightDb db, char *departureAirport, int day) {
