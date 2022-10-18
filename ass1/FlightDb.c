c3db7a21f8f32bdfc0920bb36f50af4226a4924a
Hoang Long <longpaus@gmail.com>
2022-10-19 10:19:51 +1100

    did DbFindByDepartureAirportDay

--- ass1/FlightDb.c
+++ ass1/FlightDb.c
@@ -13,6 +13,7 @@ int compareByFlightNum(Record r1, Record r2);
 
 struct flightDb {
     Tree byFlightNum;
+    Tree byDepartAp;
 };
 
 FlightDb DbNew(void) {
@@ -22,10 +23,12 @@ FlightDb DbNew(void) {
 }
 void DbFree(FlightDb db) {
     TreeFree(db->byFlightNum,true);
+    TreeFree(db->byDepartAp,true);
     free(db);
 }
 
 bool DbInsertRecord(FlightDb db, Record r) {
+    TreeInsert(db->byDepartAp,r);
 	return TreeInsert(db ->byFlightNum,r);
 }
 
@@ -39,7 +42,12 @@ List DbFindByFlightNumber(FlightDb db, char *flightNumber) {
 }
 
 List DbFindByDepartureAirportDay(FlightDb db, char *departureAirport, int day) {
-	return ListNew();
+    Record low = RecordNew("",departureAirport,"",day,0,0,0);
+    Record high = RecordNew("ZZZZZZZZ",departureAirport,"",day,23,59,0);
+    List l = TreeSearchBetween(db->byDepartAp,low,high);
+    RecordFree(low);
+    RecordFree(high);
+    return l;
 }
 
 List DbFindBetweenTimes(FlightDb db, int day1, int hour1, int min1, int day2,
@@ -75,4 +83,32 @@ int compareByFlightNum(Record r1, Record r2) {
     }
     
     return 0;
+}
+
+/*
+compare two records r1 and r2.
+comparing by departure airport then day then hour and then minute then flight number.
+returns 1 if r1 > r2, 0 if r1 == r2 and -1 if r1 < r2.
+*/
+int compareByDepartAp(Record r1, Record r2){
+    int departAp = strcmp(RecordGetDepartureAirport(r1),RecordGetDepartureAirport(r2));
+    if(departAp != 0){
+        return (departAp > 0) ? 1 : -1;
+    }
+    if(RecordGetDepartureDay(r1) != RecordGetDepartureDay(r2)){
+        return(RecordGetDepartureDay(r1) > RecordGetDepartureDay(r2)) ? 1 : -1;
+    }
+    if(RecordGetDepartureHour(r1) != RecordGetDepartureHour(r2)){
+        return (RecordGetDepartureHour(r1) > RecordGetDepartureHour(r2)) ? 1 : -1;
+    }
+    if(RecordGetDepartureMinute(r1) != RecordGetDepartureMinute(r2)){
+        return (RecordGetDepartureMinute(r1) > RecordGetDepartureMinute(r2)) ? 1 : -1;
+    }
+    int flightNumCompare =
+	    strcmp(RecordGetFlightNumber(r1), RecordGetFlightNumber(r2));
+	if (flightNumCompare != 0) {
+		return (flightNumCompare > 0) ? 1 : -1;
+	}
+    return 0;
+
 }
