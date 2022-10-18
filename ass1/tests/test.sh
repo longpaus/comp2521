ee59b7298d38dd5171fbb8f37882df34b62e58af
Hoang Long <longpaus@gmail.com>
2022-10-18 23:01:08 +1100

    wrote test2

--- ass1/tests/test.sh
+++ ass1/tests/test.sh
@@ -14,3 +14,17 @@ else
 fi
 
 rm task1Output
+
+# test 2
+.././testFlightDb 2 > task2Output
+
+diff task2Output 2.exp
+
+if test $? -eq 1 
+then
+    echo "Fail task2"
+else
+    echo "pass task2"
+fi
+
+rm task2Output
