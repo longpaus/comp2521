78daa1a57ea4f5f9ae2f1004b6af6fec8f3932d4
Hoang Long <longpaus@gmail.com>
2022-11-03 15:10:45 +1100

    fix test

--- labs/lab08/test.sh
+++ labs/lab08/test.sh
@@ -2,10 +2,12 @@
 
 for fileNum in 1 2 3 4 5 6;do
     (./testGraphMST < tests/graphMST/$fileNum.in) > testFile
-    diff -u testFile $fileNum.exp
+    diff -u testFile tests/graphMST/$fileNum.exp
     if test $? -eq 1 ;then
         echo "Fail task$fileNum"
     else
         echo "pass task$fileNum"
     fi
 done
+
+rm testFile
