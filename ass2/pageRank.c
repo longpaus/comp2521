8dc39fd70ab81fd52a9e211b5bb92e818ad47a03
Hoang Long <longpaus@gmail.com>
2022-11-12 23:16:20 +1100

    changes

--- ass2/pageRank.c
+++ ass2/pageRank.c
@@ -10,21 +10,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "List.h"
+
+List getCollection();
+
+#define MAX_URL_LEN 1024
 
 int main(int argc, char *argv[]) {
     // argc is the number of command-line arguments, which includes the
     // program name
-    // argv is the array of command-line arguments
-    if (argc != 4) {
-        fprintf(stderr, "Usage: %s dampingFactor diffPR maxIterations\n",
-                argv[0]);
-        return EXIT_FAILURE;
-    }
+    // // argv is the array of command-line arguments
+    // if (argc != 4) {
+    //     fprintf(stderr, "Usage: %s dampingFactor diffPR maxIterations\n",
+    //             argv[0]);
+    //     return EXIT_FAILURE;
+    // }
 
     // Command-line arguments are received as strings. They can be converted
     // to number types using atoi and atof.
     // double d = atof(argv[1]);
     // double diffPR = atof(argv[2]);
     // int maxIterations = atoi(argv[3]);
+    getCollection();
 }
+
+List getCollection(){
+    FILE *f = fopen("collection.txt","r");
+    char x[MAX_URL_LEN];
+    List l = ListNew();
+    while (fscanf(f, " %1023s", x) == 1) {
+        ListAppend(l,x);
+    }
+    ListPrint(l);
+    return l;
+}
+
 
