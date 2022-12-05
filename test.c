c2d013a89831bc3aa65052817b1e3200bb2ffe24
Hoang Long <longpaus@gmail.com>
2022-12-05 20:49:13 +1100

    ed

--- test.c
+++ test.c
@@ -3,7 +3,27 @@
 
 
 int main() {
-	int y = abs(-10);
-   printf("absolute value of y = %d\n", y);
+	for(int i = 0; i < 10; i++){
+      if(i == 2) i = 7;
+      printf("%d\n",i);
+   }
 
+}
+
+
+void bubbleSort(int a[], int lo, int hi)
+{
+   int i, j, nswaps;
+   for (i = lo; i < hi; i++) {
+      nswaps = 0;
+      for (j = hi; j > i; j--) {
+         if (a[j] < a[j-1]) {
+            int tmp = a[j];
+            a[j] = a[j -1];
+            a[j -1] = tmp;
+            nswaps++;
+         }
+      }
+      if (nswaps == 0) break;
+   }
 }
